cd37dca84e21acaf27b429c3cf9ce0e86b99db00
Admin <Admin@219-13>
2024-04-01 12:40:14 +0800

    commit

---
+++ murun/src/murun/bbb.java
@@ -0,0 +1,10 @@
+package murun;
+
+public class bbb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Murun");
+	}
+
+}
---
+++ murun/src/murun/lab10.java
@@ -0,0 +1,10 @@
+package murun;
+
+public class lab10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("uuy");
+	}
+
+}
